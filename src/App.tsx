9571bce4dd7ae57f50249a0cf50b95169f78a3c4
lenconda <i@lenconda.top>
2022-05-13 14:23:28 +0800

    fix: 🐛 remove unused code in src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,24 @@ import {
     useState,
 } from 'react';
 import { LocaleService } from '@modules/locale/locale.service';
-import { BrandService } from '@modules/brand/brand.service';
 import { ContainerComponent } from '@modules/container/container.component';
 import { ContainerProps } from '@modules/container/container.interface';
 import { AliveScope } from 'react-activation';
 import { ListenerComponent } from '@modules/container/listener.component';
 
 const App: FC<PropsWithChildren<InjectedComponentProps>> = ({ declarations }) => {
-    const brandService = declarations.get<BrandService>(BrandService);
     const localeService = declarations.get<LocaleService>(LocaleService);
     const Container = declarations.get<FC<ContainerProps>>(ContainerComponent);
     const Listener = declarations.get<FC>(ListenerComponent);
 
     const [locale, setLocale] = useState(localStorage.getItem('locale') || 'en_US');
-    const [logo, setLogo] = useState<string>('');
+    const localeMap = localeService.useLocaleMap(locale);
 
     const LocaleContext = localeService.getContext();
-
-    const localeMap = localeService.useLocaleMap(locale);
 
     useEffect(() => {
         localStorage.setItem('locale', locale);
     }, [locale]);
-
-    useEffect(() => {
-        if (!logo) {
-            setLogo(brandService.getLogo());
-        }
-    }, [logo]);
 
     return (
         <>
--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,6 @@
 import { lazy } from 'react';
 import { Component } from 'khamsa';
 import { LocaleService } from '@modules/locale/locale.service';
-import { BrandService } from '@modules/brand/brand.service';
-import { LocaleMenuComponent } from '@modules/locale/locale-menu.component';
-import { ProfileMenuComponent } from '@modules/profile/profile-menu.component';
-import { ClientsDropdownComponent } from '@modules/clients/clients-dropdown.component';
 import { ContainerComponent } from '@modules/container/container.component';
 import { ListenerComponent } from '@modules/container/listener.component';
 
@@ -12,10 +8,6 @@ import { ListenerComponent } from '@modules/container/listener.component';
     component: lazy(() => import('@/App')),
     declarations: [
         LocaleService,
-        BrandService,
-        LocaleMenuComponent,
-        ProfileMenuComponent,
-        ClientsDropdownComponent,
         ContainerComponent,
         ListenerComponent,
     ],
