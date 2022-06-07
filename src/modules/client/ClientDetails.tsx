ec9fa57b3dfc8e20dbb9cb5b7796cc9b929b78e4
lenconda <i@lenconda.top>
2022-06-07 23:15:11 +0800

    feat: 🎸 page optimization

--- src/modules/client/ClientDetails.tsx
+++ src/modules/client/ClientDetails.tsx
@@ -1,14 +1,36 @@
 import { FC } from 'react';
 import Box from '@mui/material/Box';
 import styled from '@mui/material/styles/styled';
+import SimpleBar from 'simplebar-react';
 
 const ClientDetailsPage = styled(Box)(() => {
-    return '';
+    return `
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+
+        .form-wrapper {
+            width: 720px;
+        }
+    `;
 });
 
 const ClientDetails: FC = () => {
     return (
-        <ClientDetailsPage></ClientDetailsPage>
+        <ClientDetailsPage>
+            <SimpleBar style={{ width: 720, height: '100%' }}>
+                <Box className="">
+                    {
+                        new Array(300).fill(null).map(() => {
+                            return <p>{Math.random().toString(32).slice(1)}</p>;
+                        })
+                    }
+                </Box>
+                <h1>EOF</h1>
+            </SimpleBar>
+        </ClientDetailsPage>
     );
 };
 
